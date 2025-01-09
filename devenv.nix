0a5a69cb8eaaf3e818bbf6eeeff8df2c84e2d2f4
Chris Simon <chris@devcycles.io>
2025-01-09 15:01:14 +1100

    Setup playwright for watching tests and src files

--- devenv.nix
+++ devenv.nix
@@ -1,3 +1,15 @@
+{ pkgs, ... }:
+
 { 
+  env.PLAYWRIGHT_BROWSERS_PATH=pkgs.playwright-driver.browsers;
+
+  packages = [
+    pkgs.nodejs_23
+    pkgs.playwright-driver
+    pkgs.watchexec
+  ];
+
   languages.typescript.enable = true;
+
+  enterShell = "code .";
 }
