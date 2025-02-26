dca3971cbb92b2fc3176dde7173a78b3fad7f537
Chris Simon <chris@chrissimon.au>
2025-02-26 12:14:39 +1100

    1 - GREEN - Enroling in a Course - add course select box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
 
         <h1>University Registration</h1>
 
+        <label htmlFor="courses">Courses</label>
+        <select id="courses"></select>
+
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 
