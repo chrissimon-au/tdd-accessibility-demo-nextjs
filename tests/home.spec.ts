0c9adbecbc12a7b762771259b8c8c8182b9d0c62
Chris Simon <chris@chrissimon.au>
2025-02-26 15:44:45 +1100

    0.1 - RED - check for page title

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,5 +1,12 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('Confirm playwright test watching', () => {
 
+});
+
+test('Page has Title', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/University Registration/);
 });
