95d180aa1420961e8fa307cffe8a34b8d2dc486f
Chris Simon <chris@chrissimon.au>
2025-02-26 17:25:45 +1100

    0.4 - RED - check for level 1 heading

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -15,7 +15,7 @@ test('Page has Title', async ({ page }) => {
 test('Page has Heading', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
-  await expect(page.getByRole('heading', { name: 'University Registration' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'University Registration', level: 1 })).toBeVisible();
 });
 
 test('Page has no Accessibility Issues', async ({ page }) => {
