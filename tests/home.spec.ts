a1777fc97a9addbb9110660c01dcdce1c524b774
Chris Simon <chris@chrissimon.au>
2025-02-26 20:04:39 +1100

    0.5 - REFACTOR - move url to baseUrl playwright setting

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -6,20 +6,20 @@ test('Confirm playwright test watching', () => {
 });
 
 test('Page has Title', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/University Registration/);
 });
 
 test('Page has Heading', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('');
 
   await expect(page.getByRole('heading', { name: 'University Registration', level: 1 })).toBeVisible();
 });
 
 test('Page has no Accessibility Issues', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('');
 
   const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
