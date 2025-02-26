eae18916f357ca79e9c98bbbef7f5341de389cb8
Chris Simon <chris@chrissimon.au>
2025-02-26 17:22:46 +1100

    0.2 - RED - check for page heading

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -10,3 +10,9 @@ test('Page has Title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/University Registration/);
 });
+
+test('Page has Heading', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await expect(page.getByRole('heading', { name: 'University Registration' })).toBeVisible();
+});
