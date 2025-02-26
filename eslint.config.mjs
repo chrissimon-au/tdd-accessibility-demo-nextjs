749412cfae39694cf510c2bc59491883ce9ed7aa
Chris Simon <chris@chrissimon.au>
2025-02-26 14:40:53 +1100

    Add eslint and @stylistic linting packages to ensure consistent styling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = tseslint.config(
+  tseslint.configs.recommended,
+  stylistic.configs.customize({
+    flat: true, // required for flat config
+    // the following options are the default values
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+    jsx: true,
+  }),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+);
 
 export default eslintConfig;
