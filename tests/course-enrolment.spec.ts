fb78ea64e0839164f1ca9c3c9f733087ddc53b32
Chris Simon <chris@chrissimon.au>
2025-02-26 17:30:49 +1100

    3 - RED - Enroling in a Course - check for required input value

--- tests/course-enrolment.spec.ts
+++ tests/course-enrolment.spec.ts
@@ -6,9 +6,11 @@ test('Enroling in a Course', async ({ page }) => {
   });
 
   await test.step('When I enrol in a course', async () => {
+    const courseId = '1ca0289a-7125-4764-bef5-ef9731554717';
     const coursesList = page.getByRole('combobox', { name: 'Courses' });
     await expect(coursesList).toBeVisible();
     await expect(coursesList.getByRole('option', { name: 'Accessibility 101' })).toBeEnabled();
+    await expect(coursesList).toHaveValue(courseId);
   });
 
   await test.step('Then I should be enroled in that course', async () => {
