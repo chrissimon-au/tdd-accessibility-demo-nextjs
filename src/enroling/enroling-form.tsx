9e812b17259dc8c3544880df5d095ff4047298a9
Chris Simon <chris@chrissimon.au>
2025-02-27 21:20:01 +1100

    9 - GREEN - Enroling in a Course - can enrol in any course

--- src/enroling/enroling-form.tsx
+++ src/enroling/enroling-form.tsx
@@ -4,22 +4,30 @@ export interface Course {
 }
 
 interface Props {
-  enrol: () => void;
+  enrol: (course: Course) => void;
   courses: Array<Course>;
 }
 
 const DEFAULT_COURSES = [{ name: 'Accessibility 101', id: '1ca0289a-7125-4764-bef5-ef9731554717' }];
 
 export default function EnrolingForm({ enrol, courses }: Props) {
+  function submitEnrol(formData: FormData) {
+    const courseId = formData.get('course') as string;
+    const course = {
+      id: courseId ?? '',
+      name: courses.find(c => c.id == courseId)?.name ?? 'Course not found',
+    };
+    enrol(course);
+  }
   return (
-    <>
-      <label htmlFor="courses">Courses</label>
-      <select id="courses">
+    <form action={submitEnrol}>
+      <label htmlFor="course">Courses</label>
+      <select id="course" name="course">
         { (courses ?? DEFAULT_COURSES).map(course =>
           <option key={course.id} value={course.id}>{course.name}</option>,
         )}
       </select>
-      <button onClick={enrol}>Enrol</button>
-    </>
+      <button>Enrol</button>
+    </form>
   );
 }
