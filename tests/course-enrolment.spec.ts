3b519aa1b7aac4c545f535a93a4cac3da58a578a
Chris Simon <chris@chrissimon.au>
2025-02-27 20:58:14 +1100

    7 - RED - Enroling in a Course - can see all available courses in list with mock API call

--- tests/course-enrolment.spec.ts
+++ tests/course-enrolment.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
 test('Enroling in a Course', async ({ page }) => {
   await test.step('Given I am a registered student', async () => {
@@ -21,4 +21,32 @@ test('Enroling in a Course', async ({ page }) => {
     await expect(enrolments).toBeVisible();
     await expect(enrolments.getByRole('cell', { name: 'Accessibility 101' })).toBeVisible();
   });
+});
+
+test('Reviewing available Courses', async ({ page }) => {
+  const allCourses = [
+    { name: 'Accessibility 101', id: '1ca0289a-7125-4764-bef5-ef9731554717' },
+    { name: 'Front End Development 201', id: '260081c3-57b4-4d79-bbcb-4e7c43b31d3b' },
+    { name: 'Test-Driven Development 101', id: 'a9a2fe0d-f8bf-4201-9684-b22e4c42d21d' },
+  ];
+
+  await page.route('*/**/courses', async (route) => {
+    await route.fulfill({ json: allCourses });
+  });
+
+  await test.step('Given I am a registered student', async () => {
+    await page.goto('');
+  });
+
+  let coursesList: Locator;
+
+  await test.step('When I review available courses', async () => {
+    coursesList = page.getByRole('combobox', { name: 'Courses' });
+  });
+
+  for (const course of allCourses) {
+    await test.step(`Then I should see course ${course.name} in list`, async () => {
+      await expect(coursesList.getByRole('option', { name: course.name })).toBeEnabled();
+    });
+  }
 });
