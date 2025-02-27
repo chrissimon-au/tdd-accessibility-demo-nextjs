2662d49c013b25ec72b4bcba966c53deb2a12c81
Chris Simon <chris@chrissimon.au>
2025-02-27 20:58:40 +1100

    7 - GREEN - Enroling in a Course - can see all available courses in list with default value to preserve tests without mock config

--- src/enroling/enroling-form.tsx
+++ src/enroling/enroling-form.tsx
@@ -1,13 +1,23 @@
-interface Props {
-  enrol: () => void;
+export interface Course {
+  name: string;
+  id: string;
 }
 
-export default function EnrolingForm({ enrol }: Props) {
+interface Props {
+  enrol: () => void;
+  courses: Array<Course>;
+}
+
+const DEFAULT_COURSES = [{ name: 'Accessibility 101', id: '1ca0289a-7125-4764-bef5-ef9731554717' }];
+
+export default function EnrolingForm({ enrol, courses }: Props) {
   return (
     <>
       <label htmlFor="courses">Courses</label>
       <select id="courses">
-        <option value="1ca0289a-7125-4764-bef5-ef9731554717">Accessibility 101</option>
+        { (courses ?? DEFAULT_COURSES).map(course =>
+          <option key={course.id} value={course.id}>{course.name}</option>,
+        )}
       </select>
       <button onClick={enrol}>Enrol</button>
     </>
--- src/enroling/enroling.tsx
+++ src/enroling/enroling.tsx
@@ -1,15 +1,31 @@
 'use client';
-import React from 'react';
-import EnrolingForm from '@/enroling/enroling-form';
+import { useEffect, useState } from 'react';
+import EnrolingForm, { Course } from '@/enroling/enroling-form';
 import Enrolments from '@/enroling/enrolments';
 
 export default function Enroling() {
-  const [enrolments, setEnrolments] = React.useState([] as Array<string>);
+  const [enrolments, setEnrolments] = useState([] as Array<string>);
   const enrol = () => setEnrolments(['Accessibility 101']);
+
+  const [courses, setCourses] = useState([] as Array<Course>);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const response = await fetch(
+        `/courses`,
+      );
+
+      const courses = response.ok ? await response.json() : null;
+
+      setCourses(courses);
+    };
+
+    fetchCourses();
+  }, [courses]);
 
   return (
     <>
-      <EnrolingForm enrol={enrol} />
+      <EnrolingForm enrol={enrol} courses={courses} />
       <Enrolments enrolments={enrolments} />
     </>
   );
