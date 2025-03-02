fde3052bbf9ce6d24afb6f2a8b5788c8b5568d7f
Chris Simon <chris@chrissimon.au>
2025-03-02 14:29:41 +1100

    6 - REFACTOR - Enroling in a Course - prep enroling form to accept available courses as a prop

--- src/enroling/enroling-form.tsx
+++ src/enroling/enroling-form.tsx
@@ -1,13 +1,19 @@
+export interface Course {
+  name: string;
+  id: string;
+}
+
 interface Props {
   onEnrol: () => void;
 }
 
 export default function EnrolingForm({ onEnrol }: Props) {
+  const course = { id: '1ca0289a-7125-4764-bef5-ef9731554717', name: 'Accessibility 101' };
   return (
     <>
       <label htmlFor="courses">Courses</label>
       <select id="courses">
-        <option value="1ca0289a-7125-4764-bef5-ef9731554717">Accessibility 101</option>
+        <option value={course.id}>{course.name}</option>
       </select>
       <button onClick={onEnrol}>Enrol</button>
     </>
