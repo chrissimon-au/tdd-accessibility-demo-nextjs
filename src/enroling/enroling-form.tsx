8867e90d89de332eb592be07480376d6900d2c27
Chris Simon <chris@chrissimon.au>
2025-03-02 14:53:59 +1100

    8 - GREEN - Enroling in a Course - enroling now enrols in the selected course

--- src/enroling/enroling-form.tsx
+++ src/enroling/enroling-form.tsx
@@ -4,19 +4,20 @@ export interface Course {
 }
 
 interface Props {
-  onEnrol: () => void;
+  onEnrol: (course: Course) => void;
   courses: Array<Course>;
 }
 
 export default function EnrolingForm({ onEnrol, courses }: Props) {
   const course = courses[0];
+  const onEnrolClick = () => onEnrol(course);
   return course && (
     <>
       <label htmlFor="courses">Courses</label>
       <select id="courses">
         <option value={course.id}>{course.name}</option>
       </select>
-      <button onClick={onEnrol}>Enrol</button>
+      <button onClick={onEnrolClick}>Enrol</button>
     </>
   );
 }
