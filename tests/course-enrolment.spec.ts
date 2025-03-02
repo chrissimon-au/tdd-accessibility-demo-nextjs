3af559f37fc55686b55d73387ee0a2c742bb65e3
Chris Simon <chris@chrissimon.au>
2025-03-02 14:38:16 +1100

    6 - REFACTOR - Enroling in a Course - enroling test is now parameterised in prep for testing different courses

--- tests/course-enrolment.spec.ts
+++ tests/course-enrolment.spec.ts
@@ -1,24 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test('Enroling in a Course', async ({ page }) => {
-  await test.step('Given I am a registered student', async () => {
-    await page.goto('');
-  });
+const allCourses = [
+  { name: 'Accessibility 101', id: '1ca0289a-7125-4764-bef5-ef9731554717' },
+];
 
-  await test.step('When I enrol in a course', async () => {
-    const courseId = '1ca0289a-7125-4764-bef5-ef9731554717';
-    const coursesList = page.getByRole('combobox', { name: 'Courses' });
-    await expect(coursesList).toBeVisible();
-    await expect(coursesList.getByRole('option', { name: 'Accessibility 101' })).toBeEnabled();
-    await expect(coursesList).toHaveValue(courseId);
-    const enrolButton = page.getByRole('button', { name: 'Enrol' });
-    await expect(enrolButton).toBeEnabled();
-    await enrolButton.click();
-  });
+test.describe('Enroling in a Course', () => {
+  for (const course of allCourses) {
+    test(`Enroling in ${course.name}`, async ({ page }) => {
+      await test.step('Given I am a registered student', async () => {
+        await page.goto('');
+      });
 
-  await test.step('Then I should be enroled in that course', async () => {
-    const enrolments = page.getByRole('table', { name: 'Enrolments' });
-    await expect(enrolments).toBeVisible();
-    await expect(enrolments.getByRole('cell', { name: 'Accessibility 101' })).toBeVisible();
-  });
+      await test.step('When I enrol in a course', async () => {
+        const coursesList = page.getByRole('combobox', { name: 'Courses' });
+        await expect(coursesList).toBeVisible();
+        await expect(coursesList.getByRole('option', { name: course.name })).toBeEnabled();
+        await expect(coursesList).toHaveValue(course.id);
+        const enrolButton = page.getByRole('button', { name: 'Enrol' });
+        await expect(enrolButton).toBeEnabled();
+        await enrolButton.click();
+      });
+
+      await test.step('Then I should be enroled in that course', async () => {
+        const enrolments = page.getByRole('table', { name: 'Enrolments' });
+        await expect(enrolments).toBeVisible();
+        await expect(enrolments.getByRole('cell', { name: 'Accessibility 101' })).toBeVisible();
+      });
+    });
+  }
 });
