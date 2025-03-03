1cf17151084ddaf2fb38273a5cb80e269f881352
Chris Simon <chris@chrissimon.au>
2025-03-03 11:05:45 +1100

    13 - VISUAL - Enroling in a Course - visual layout of enrolments list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Enroling from '@/enroling/enroling';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-[50%] mx-24">
 
         <h1 className="text-3xl">University Registration</h1>
 
--- src/enroling/enrolments.tsx
+++ src/enroling/enrolments.tsx
@@ -4,13 +4,13 @@ interface Props {
 
 export default function Enrolments({ enrolments }: Props) {
   return (
-    <table>
-      <caption>Enrolments</caption>
+    <table className="w-full table-fixed">
+      <caption className="text-left text-2xl">Enrolments</caption>
       <thead>
         <tr>
-          <th>Course</th>
-          <th>Status</th>
-          <th>Room</th>
+          <th className="text-left m-2 w-[50%]">Course</th>
+          <th className="text-left m-2">Status</th>
+          <th className="text-left m-2">Room</th>
         </tr>
       </thead>
       <tbody>
