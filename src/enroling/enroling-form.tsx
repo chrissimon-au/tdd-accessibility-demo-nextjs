22d58327f0b83e8962f88d082fbcc63a290b813e
Chris Simon <chris@chrissimon.au>
2025-03-03 11:03:06 +1100

    13 - VISUAL - Enroling in a Course - refactor tailwindcss for visual layout of the enroling form

--- src/enroling/enroling-form.tsx
+++ src/enroling/enroling-form.tsx
@@ -20,14 +20,18 @@ export default function EnrolingForm({ onEnrol, courses }: Props) {
   const helpMsg = `Select the course you'd like to enrol in...`;
   return courses && (
     <form action={enrol}>
-      <label htmlFor="course">Courses</label>
-      <select id="course" name="course" defaultValue={helpMsg}>
-        <option disabled>{ helpMsg }</option>
-        { courses.map(course =>
-          <option key={`course-${course.id}`} value={course.id}>{course.name}</option>,
-        )}
-      </select>
-      <button>Enrol</button>
+      <div>
+        <label htmlFor="course" className="block mb-3 text-2xl">Courses</label>
+        <select id="course" name="course" defaultValue={helpMsg} className="block mb-3 px-2 py-1 border rounded-md border-gray-200 bg-white">
+          <option disabled>{helpMsg}</option>
+          {courses.map(course =>
+            <option key={`course-${course.id}`} value={course.id}>{course.name}</option>,
+          )}
+        </select>
+      </div>
+      <div>
+        <button className="px-3 py-1 rounded-md bg-blue-600 text-white">Enrol</button>
+      </div>
     </form>
   );
 }
