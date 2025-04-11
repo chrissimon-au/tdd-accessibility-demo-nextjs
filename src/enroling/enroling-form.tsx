6dd92e7270ba554757c1ec5a1713978e6e40a14a
Chris Simon <chris@chrissimon.au>
2025-04-11 13:18:54 +1000

    17 - VISUAL - style the persistent error message

--- src/enroling/enroling-form.tsx
+++ src/enroling/enroling-form.tsx
@@ -32,19 +32,19 @@ export default function EnrolingForm({ onEnrol, courses }: Props) {
     <form action={enrol}>
       <div>
         <label htmlFor="course" className="block mb-3 text-2xl">Courses</label>
-        <select id="course" onInvalid={onInvalid} aria-describedby="course-errors" name="course" required defaultValue="" className="block mb-3 px-2 py-1 border rounded-md border-gray-200 bg-white">
+        <select id="course" onInvalid={onInvalid} aria-describedby="course-errors" name="course" required defaultValue="" className="block px-2 py-1 border rounded-md border-gray-200 bg-white">
           <option value="" disabled>{helpMsg}</option>
           {courses.map(course =>
             <option key={`course-${course.id}`} value={course.id}>{course.name}</option>,
           )}
         </select>
         {isInvalid && (
-          <div id="course-errors">
+          <div id="course-errors" className="bg-error px-3 py-1 pt-none text-sm rounded-lg rounded-tl-none rounded-tr-none">
             Please select the course to enrol in.
           </div>
         )}
       </div>
-      <div>
+      <div className="mt-3">
         <button className="px-3 py-1 rounded-md bg-blue-600 text-white">Enrol</button>
       </div>
     </form>
