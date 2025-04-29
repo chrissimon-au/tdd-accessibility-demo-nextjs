6bab6ee5692747ce59a91cfbf2e94bf139d97bf8
Chris Simon <chris@devcycles.io>
2025-04-30 09:49:10 +1000

    7 - RED - assert on presence of extra info about the enrolment in the table

--- tests/course-enrolment.spec.ts
+++ tests/course-enrolment.spec.ts
@@ -19,6 +19,22 @@ test('Enroling in a Course', async ({ page }) => {
   await test.step('Then I should be enroled in that course', async () => {
     const enrolments = page.getByRole('table', { name: 'Enrolments' });
     await expect(enrolments).toBeVisible();
-    await expect(enrolments.getByRole('cell', { name: 'Accessibility 101' })).toBeVisible();
+
+    const headers = enrolments.getByRole('rowgroup').nth(0).getByRole('cell');
+    await expect(headers).toHaveCount(3);
+    const headerCellsContent = await Promise.all((await headers.all()).map(l => l.textContent()));
+    const courseIdx = headerCellsContent.indexOf('Course');
+    const statusIdx = headerCellsContent.indexOf('Status');
+    const roomIdx = headerCellsContent.indexOf('Room');
+
+    const enrolmentCourse = enrolments.getByRole('row', { name: 'Accessibility 101' });
+    await expect(enrolmentCourse).toBeVisible();
+    const enrolmentCourseCells = enrolmentCourse.getByRole('cell');
+    const course = enrolmentCourseCells.nth(courseIdx);
+    const status = enrolmentCourseCells.nth(statusIdx);
+    const room = enrolmentCourseCells.nth(roomIdx);
+    await expect(course).toHaveText('Accessibility 101');
+    await expect(status).toHaveText('Submitted');
+    await expect(room).toHaveText('Not yet allocated');
   });
 });
