d0e3b5d7125339ed95e5329453fb08e9acf4b8c9
Chris Simon <chris@devcycles.io>
2025-04-30 09:49:25 +1000

    7 - GREEN - add extra table headers and cells

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,11 +19,20 @@ export default function Home() {
 
         <table>
           <caption>Enrolments</caption>
+          <thead>
+            <tr>
+              <th>Course</th>
+              <th>Status</th>
+              <th>Room</th>
+            </tr>
+          </thead>
           <tbody>
             {enrolments.map((enrolment, idx) => {
               return (
                 <tr key={`enrolment-${idx}`}>
                   <td>{enrolment}</td>
+                  <td>Submitted</td>
+                  <td>Not yet allocated</td>
                 </tr>
               );
             })}
